161a7b8a118b08e071c369e9547ee3f2c205c857
Kamal Thapa <kamalsoftworks@gmail.com>
2026-06-03 02:50:57 +0545

    recursion/palindrome

---
+++ Patterns/recursion11.cpp
@@ -0,0 +1,38 @@
+// to check if a string is palindrome
+#include<iostream>
+using namespace std;
+bool f(int i, string &s)
+{
+    if(i>=s.size()/2)
+    {
+        return true;
+    }
+    if(s[i]!=s[s.size()-i-1])
+    {
+        return false;
+    }
+    return f(i+1, s);
+}
+int main()
+{
+    string s;
+
+    cout << "Enter a string: ";
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++)
+    {
+        s[i] = tolower(s[i]);
+    }
+
+    if(f(0, s))
+    {
+        cout << "Palindrome";
+    }
+    else
+    {
+        cout << "Not Palindrome";
+    }
+
+    return 0;
+}
