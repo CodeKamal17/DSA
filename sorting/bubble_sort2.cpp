1a4308ddcfafdda94afd45c3eaa1d42d83a0297f
Kamal Thapa <kamalsoftworks@gmail.com>
2026-06-08 01:23:22 +0545

    bubble_sort

--- sorting/bubble_sort2.cpp
+++ sorting/bubble_sort2.cpp
@@ -4,8 +4,10 @@ using namespace std;
 void selection_sort(int arr[], int n)
 {
     for(int i = n-1; i>=1; i--)
+    // for(int i = 0; i<n-1; i++)
     {
         for(int j = 0; j<=i-1; j++)
+        // for(int j = 0; j<n-i-1; j++)
         {
             if(arr[j] < arr[j+1])
             {
