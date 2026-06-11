2acb95424e9209699a1bb87b2bfcf325b0240bb6
Kamal Thapa <kamalsoftworks@gmail.com>
2026-06-12 02:52:10 +0545

    Finding second largest element in optimal way

---
+++ array/secondLargest3.cpp
@@ -0,0 +1,53 @@
+// Finding second largest element better way
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+void seclargestElement(vector <int> &arr, int n)
+{
+    int largest = arr[0];
+    int seclargest = INT_MIN;
+
+    for(int i = 0; i<n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            seclargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > seclargest)
+        {
+            seclargest = arr[i];
+        }
+    }
+    if (seclargest == INT_MIN) 
+    {
+        cout << "None (all elements are equal)";
+    } 
+    else 
+    {
+        cout << seclargest;
+    }
+    
+}
+int main()
+{
+    int size;
+    cout<<"Enter array size(>1): ";
+    cin>>size;
+    
+
+    vector<int> arr(size);
+    cout<<"Enter array: "<<endl;
+    for(int i = 0; i<size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Second largest element in the array is: ";
+    seclargestElement(arr, size);
+    return 0;
+
+}
+
